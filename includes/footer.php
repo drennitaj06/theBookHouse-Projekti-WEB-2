59e889218d8443a117dafda2634d2cd327917cd6
drennitaj06 <dren.nitaj@student.uni-pr.edu>
2026-04-20 20:02:47 +0200

    Add footer and adjust header asset paths
    Add a new includes/footer.php containing site footer markup (Pages, Legal, social links) and a script include for ../assets/js/script.js, plus closing </body> and </html> tags. Update includes/header.php to use relative asset paths for the stylesheet and logo image (change /assets/... to ../assets/...), noting the logo src was changed to ../assests/images/logo1.png (possible typo in "assests").

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<footer class="site-footer">
+    <div class="footer-container">
+
+        <div class="footer-pages-legal">
+            <div class="footer-menu">
+                <h3>Pages</h3>
+                <ul>
+                    <li><a href="/index.php">Home</a></li>
+                    <li><a href="/pages/books.php">Books</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-menu">
+                <h3>Legal</h3>
+                <ul>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Privacy</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-social">
+            <h3>Follow Us</h3>
+            <ul>
+                <li><a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="https://x.com/"><i class="fa-brands fa-x-twitter"></i></a></li>
+                <li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
+                <li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a></li>
+                <li><a href="https://www.tiktok.com/"><i class="fab fa-tiktok"></i></a></li>
+            </ul>
+        </div>
+
+    </div>
+</footer>
+
+<script src="../assets/js/script.js"></script>
+</body>
+</html>
