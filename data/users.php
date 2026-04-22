2541daa60b63fda28593b5944cfbe792eccbfc53
nati-hs <donat.hasani@student.uni-pr.edu>
2026-04-22 15:28:06 +0200

    add static users list with admin/user roles

---
+++ data/users.php
@@ -0,0 +1,28 @@
+<?php
+
+$users = [
+    [
+        'id' => 1,
+        'name' => 'Admin',
+        'surname' => 'User',
+        'username' => 'admin',
+        'email' => 'admin@bookhouse.com',
+        'password' => 'admin123', // later: password_hash()
+        'phone' => '',
+        'address' => '',
+        'role' => 'admin'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Dren',
+        'surname' => 'Nitaj',
+        'username' => 'dren',
+        'email' => 'user@bookhouse.com',
+        'password' => 'user123',
+        'phone' => '049123456',
+        'address' => 'Prishtine',
+        'role' => 'user'
+    ]
+];
+
+?>
