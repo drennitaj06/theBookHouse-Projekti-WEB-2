9adaa2800175fd6c64c51ebc0b947c8192fc583f
nati-hs <donat.hasani@student.uni-pr.edu>
2026-04-22 15:34:15 +0200

    Added categories of books

---
+++ data/categories.php
@@ -0,0 +1,16 @@
+<?php
+
+$categories = [
+    ['category_id' => 1, 'name' => 'Fiction'],
+    ['category_id' => 2, 'name' => 'Non-Fiction'],
+    ['category_id' => 3, 'name' => 'Young Adult'],
+    ['category_id' => 4, 'name' => "Children's"],
+    ['category_id' => 5, 'name' => 'Graphic Novels & Comics'],
+    ['category_id' => 6, 'name' => 'Poetry'],
+    ['category_id' => 7, 'name' => 'Drama & Plays'],
+    ['category_id' => 8, 'name' => 'Religious & Spiritual'],
+    ['category_id' => 9, 'name' => 'Educational & Academic'],
+    ['category_id' => 10, 'name' => 'Additional Categories']
+];
+
+?>
