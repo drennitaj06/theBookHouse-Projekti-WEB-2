43c7561fdeeac252e2a2cc7d2f07d9f3a547b333
nati-hs <donat.hasani@student.uni-pr.edu>
2026-04-22 15:14:29 +0200

    Add static list of authors

---
+++ data/authors.php
@@ -0,0 +1,29 @@
+<?php
+
+$authors = [
+    ['author_id' => 1, 'name' => 'J.K. Rowling'],
+    ['author_id' => 2, 'name' => 'E.B. White'],
+    ['author_id' => 3, 'name' => 'William Shakespeare'],
+    ['author_id' => 4, 'name' => 'Arthur Miller'],
+    ['author_id' => 5, 'name' => 'Plato'],
+    ['author_id' => 6, 'name' => 'Matthew Walker'],
+    ['author_id' => 7, 'name' => 'Jane Austen'],
+    ['author_id' => 8, 'name' => 'Colson Whitehead'],
+    ['author_id' => 9, 'name' => 'Marjane Satrapi'],
+    ['author_id' => 10, 'name' => 'Frank Miller'],
+    ['author_id' => 11, 'name' => 'Anne Frank'],
+    ['author_id' => 12, 'name' => 'Trevor Noah'],
+    ['author_id' => 13, 'name' => 'Homer'],
+    ['author_id' => 14, 'name' => 'Lao Tzu'],
+    ['author_id' => 15, 'name' => 'Eckhart Tolle'],
+    ['author_id' => 16, 'name' => 'Lucy Maud Montgomery'],
+    ['author_id' => 17, 'name' => 'Tomi Adeyemi'],
+    ['author_id' => 18, 'name' => 'T. S. Eliot'],
+    ['author_id' => 19, 'name' => 'Gustave Flaubert'],
+    ['author_id' => 20, 'name' => 'Erin Morgenstern'],
+    ['author_id' => 21, 'name' => 'Fyodor Dostoyevsky'],
+    ['author_id' => 22, 'name' => 'Noel Malcolm'],
+    ['author_id' => 23, 'name' => 'Charles Dickens']
+];
+
+?>
