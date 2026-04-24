3646b70cc0ae9631f9058b6549f913d3240f7387
drennitaj06 <dren.nitaj@student.uni-pr.edu>
2026-04-24 13:54:46 +0200

    Use hashed passwords and add new user
    Replaces the previous users list with an explicit array() structure that stores bcrypt-hashed passwords instead of plaintext. Adds a third user (id=3, Donat Hasani, username `donati`) and preserves the existing admin and user accounts; this change improves credential security by using hashed password values.

--- data/users.php
+++ data/users.php
@@ -1,28 +1,42 @@
 <?php
 
-$users = [
-    [
-        'id' => 1,
-        'name' => 'Admin',
-        'surname' => 'User',
-        'username' => 'admin',
-        'email' => 'admin@bookhouse.com',
-        'password' => 'admin123', // later: password_hash()
-        'phone' => '',
-        'address' => '',
-        'role' => 'admin'
-    ],
-    [
-        'id' => 2,
-        'name' => 'Dren',
-        'surname' => 'Nitaj',
-        'username' => 'dren',
-        'email' => 'user@bookhouse.com',
-        'password' => 'user123',
-        'phone' => '049123456',
-        'address' => 'Prishtine',
-        'role' => 'user'
-    ]
-];
+$users = array (
+  0 => 
+  array (
+    'id' => 1,
+    'name' => 'Admin',
+    'surname' => 'User',
+    'username' => 'admin',
+    'email' => 'admin@bookhouse.com',
+    'password' => '$2y$10$fMamHjQStOxkBpk58QYt2OlBTG.Xpi0c2DhGEqRZal4QnwKewJuE.',
+    'phone' => '',
+    'address' => '',
+    'role' => 'admin',
+  ),
+  1 => 
+  array (
+    'id' => 2,
+    'name' => 'Dren',
+    'surname' => 'Nitaj',
+    'username' => 'dren',
+    'email' => 'user@bookhouse.com',
+    'password' => '$2y$10$1bjnPPDQ4mw0eRpQ8MwJ8.cHxzbo1PtETTkk6bYyyMD02SAsOoDa2',
+    'phone' => '049123456',
+    'address' => 'Prishtine',
+    'role' => 'user',
+  ),
+  2 => 
+  array (
+    'id' => 3,
+    'name' => 'Donat',
+    'surname' => 'Hasani',
+    'username' => 'donati',
+    'email' => 'donat@email.com',
+    'password' => '$2y$10$Y9JvI.9roORd27FNnBy60u11jNCt3ZxQs4P0iwhovk8euMMSgZQQq',
+    'phone' => '',
+    'address' => '',
+    'role' => 'user',
+  ),
+);
 
 ?>
