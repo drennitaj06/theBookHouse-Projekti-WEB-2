36e462cebdb4c03ba94ab39ac0a4d56126f84be5
Drini Gashi <drini.gashi2@student.uni-pr.edu>
2026-04-25 15:53:32 +0200

    added sessioncheck

---
+++ auth/sessionCheck.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/sessionCheck.php';
+
+function requireAdmin() {
+    requireLogin();
+    enforceSessionTimeout();
+
+    if ($_SESSION['user']['role'] !== 'admin') {
+        header("Location: " . BASE_URL . "pages/books.php");
+        exit();
+    }
+}
